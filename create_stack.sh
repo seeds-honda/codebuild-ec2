85cd068319d83d32535db18393cf75b63bd9d5e6
kotawonism <k.honda@seeds-std.co.jp>
2022-02-09 16:37:34 +0900

    追加

--- create_stack.sh
+++ create_stack.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-stack_number=$1
+START_CNT=$1
+END_CNT=$2
 
-for i in $(seq 1 $stack_number); do
+for i in $(seq $START_CNT $END_CNT); do
     aws cloudformation create-stack --stack-name ec2-test-${i} --template-body file://runec2.yml --parameters ParameterKey=InstanceName,ParameterValue=ec2-test-${i}
 done
