8bb6e3d03aee0088934a31e4b97111413071d318
kotawonism <k.honda@seeds-std.co.jp>
2022-03-02 15:20:22 +0900

    hennkou

--- create_stack.sh
+++ create_stack.sh
@@ -12,7 +12,6 @@ if [ "$START_CNT" != "$CHAR_START_CNT" ]; then
         START_CNT=1
 fi
 
-echo $START_CNT
 CHAR_END_CNT=$(expr "$END_CNT" : '\([0-9][0-9]*\)')
 if [ "$END_CNT" != "$CHAR_END_CNT" ]; then
     echo "END_CNT変数に数値以外の文字が入っています"
@@ -20,6 +19,5 @@ if [ "$END_CNT" != "$CHAR_END_CNT" ]; then
 fi
 
 for i in $(seq $START_CNT $END_CNT); do
-    #aws cloudformation create-stack --stack-name ec2-test-${i} --template-body file://kpi-ec2.yml --parameters ParameterKey=AMIID,ParameterValue=${AMI} ParameterKey=InstanceName,ParameterValue=ec2-test-${i}
-    echo $i
+    aws cloudformation create-stack --stack-name ec2-test-${i} --template-body file://kpi-ec2.yml --parameters ParameterKey=AMIID,ParameterValue=${AMI} ParameterKey=InstanceName,ParameterValue=ec2-test-${i}
 done
