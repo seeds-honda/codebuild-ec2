e93d8c0c87d0a5b402486f983d397b74f621b075
kotawonism <k.honda@seeds-std.co.jp>
2022-03-03 12:13:30 +0900

    修正

--- create_stack.sh
+++ create_stack.sh
@@ -5,6 +5,8 @@ END_CNT=$2
 AMI=$3
 INSTANCETYPE=$4
 
+INSTANCENAME=ec2-test-
+
 CHAR_START_CNT=$(expr "$START_CNT" : '\([0-9][0-9]*\)')
 if [ "$START_CNT" != "$CHAR_START_CNT" ]; then
     echo "START_CNT変数に数値以外の文字が入っています"
@@ -19,11 +21,11 @@ if [ "$END_CNT" != "$CHAR_END_CNT" ]; then
     exit 1
 fi
 
-for i in $(seq $START_CNT $END_CNT); do
-    EXITING_CNT=`aws ec2 describe-instances --filter "Name=tag:Name,Values=ec2-test-${i}" "Name=instance-state-name,Values=running" | jq -r .Reservations[].Instances[].InstanceId | wc -l`
+for i in $(seq 0$START_CNT 0$END_CNT); do
+    EXITING_CNT=`aws ec2 describe-instances --filter "Name=tag:Name,Values=${INSTANCENAME}0${i}" "Name=instance-state-name,Values=running" | jq -r .Reservations[].Instances[].InstanceId | wc -l`
     if (( $EXITING_CNT == 0 )); then
-        aws cloudformation create-stack --stack-name ec2-test-${i} --template-body file://kpi-ec2.yml \
-        --parameters ParameterKey=InstanceName,ParameterValue=ec2-test-${i} \
+        aws cloudformation create-stack --stack-name ec2-test-0${i} --template-body file://kpi-ec2.yml \
+        --parameters ParameterKey=InstanceName,ParameterValue=ec2-test-0${i} \
         ParameterKey=AMIID,ParameterValue=${AMI} \
         ParameterKey=InstanceType,ParameterValue=${INSTANCETYPE} \
         --capabilities CAPABILITY_NAMED_IAM
